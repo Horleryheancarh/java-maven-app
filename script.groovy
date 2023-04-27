85349876284a085313afbfbc3131652aea71fd8e
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-04-27 18:31:49 +0100

    disable push to nexus

--- script.groovy
+++ script.groovy
@@ -12,9 +12,10 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t 13.40.179.85:8083/java-maven-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 13.40.179.85:8083"
-        sh 'docker push 13.40.179.85:8083/java-maven-app:jma-2.0'
+        echo "$USER $PASS"
+        // sh 'docker build -t 13.40.179.85:8083/java-maven-app:jma-2.0 .'
+        // sh "echo $PASS | docker login -u $USER --password-stdin 13.40.179.85:8083"
+        // sh 'docker push 13.40.179.85:8083/java-maven-app:jma-2.0'
     }
 } 
 
