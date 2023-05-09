a0ecf604af63c4c4893ca740d384f4c69fae34ec
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-05-09 14:57:19 +0100

    autoupdate version in repo

--- script.groovy
+++ script.groovy
@@ -33,4 +33,21 @@ def deployApp() {
     echo 'deploying the application...'
 } 
 
+def versionUpdate() {
+  withCredentials([usernamePassword(credentialsId: 'github-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      sh 'git config --global user.email "jenkins@example.com"'
+      sh 'git config --global user.name "jenkins"'
+
+      sh 'git status'
+      sh 'git branch'
+      sh 'git config --list'
+
+      echo "$USER $PASS"
+      sh "git remote set-url origin https://${USER}:${PASS}@github.com/Horleryheancarh/java-maven-app.git"
+      sh 'git add .'
+      sh 'git commit -m "ci: version update"'
+      sh 'git push origin HEAD:jenkins-jobs'
+    }
+}
+
 return this
